603354f48d680b5800d3147615f3aa59edf98a38
ktanguy <t.kwizera@alustudent.com>
2024-03-02 21:43:02 +0000

    bug fix

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,13 +1,4 @@
-#!/usr/bin/python3
-"""Queries the Reddit API and
-returns the number of subscribers
-(not active users, total subscribers)
-for a given subreddit.
-If an invalid subreddit is given,
-the function should return 0.
-"""
 import requests
-
 
 def number_of_subscribers(subreddit):
     """Returns the total number of subscribers
@@ -18,16 +9,18 @@ def number_of_subscribers(subreddit):
     api_uri = '{base}/r/{subreddit}/about.json'.format(base=base_url,
                                                        subreddit=subreddit)
 
-    # Set an User-Agent
+    # Set a User-Agent
     user_agent = {'User-Agent': 'Python/requests'}
 
     # Get the Response of the Reddit API
-    res = requests.get(api_uri, headers=user_agent,
-                       allow_redirects=False)
+    res = requests.get(api_uri, headers=user_agent, allow_redirects=False)
 
     # Checks if the subreddit is invalid
     if res.status_code in [302, 404]:
         return 0
+
+    # Print out the JSON response for debugging
+    print(res.json())
 
     # Returns the total subscribers of the subreddit
     return res.json().get('data').get('subscribers')
